0499c5da88dce001d4176b21244559f5e3290aac
name-CrystalHoWT <crystal.ho@willowtreeapps.com>
2021-07-20 11:47:44 -0400

    Created buttons to go to next image

--- Gift Exchange/DetailViewController.swift
+++ Gift Exchange/DetailViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class DetailViewController:UIViewController{
     let myImageView=UIImageView()
+    var currentIndex=0
+    var UIImages:[UIImage]=[]
+    let backButton=UIButton()
+    let nextButton=UIButton()
     let myButton=UIButton()
     let myDescription=UILabel()
     let myTitle=UILabel()
@@ -29,6 +33,7 @@ class DetailViewController:UIViewController{
     }
     func setUp(){
         setUpImage()
+        setUpImageButtons()
         setUpStackView()//order matters for set up
         setUpTitle()
         setUpDescription()
@@ -47,15 +52,65 @@ class DetailViewController:UIViewController{
         EtsyAPI().getImages(id: listing.listing_id, completion: {images in
             DispatchQueue.main.async { //willswitch to main thread
                 // Create URL into UIImage
-                let imageUrlString = images[0].url_fullxfull
-                let imageUrl = URL(string: imageUrlString)!
-                let imageData = try! Data(contentsOf: imageUrl)
-                let image = UIImage(data: imageData)
-                self.myImageView.image = image
+               
+                var circleLayers:[CAShapeLayer]=[]
+                for image in images{
+                    let imageUrlString = image.url_fullxfull
+                    let imageUrl = URL(string: imageUrlString)!
+                    let imageData = try! Data(contentsOf: imageUrl)
+                    let myUIImage = UIImage(data: imageData)
+                    self.UIImages.append(myUIImage!)
+                }
+                self.myImageView.image = self.UIImages[self.currentIndex]
                 }
             }
         )
     }
+    func setUpImageButtons(){
+        self.view.addSubview(backButton)
+        self.view.addSubview(nextButton)
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        backButton.setTitleColor(.black, for: .normal)
+        backButton.setTitle("<", for: .normal)
+        nextButton.setTitleColor(.black, for: .normal)
+        nextButton.setTitle(">", for: .normal)
+        NSLayoutConstraint.activate([
+            nextButton.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            backButton.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+        ])
+        
+        nextButton.addAction(.init(handler: {[weak self]_ in
+            guard let self = self else {
+                return
+            }
+            self.currentIndex+=1
+            if self.currentIndex == self.UIImages.count{
+                self.currentIndex=0
+            }
+            else if self.currentIndex == -1{
+                self.currentIndex=self.UIImages.count-1
+            }
+            self.myImageView.image = self.UIImages[self.currentIndex]
+        }), for: .touchUpInside)
+        
+        backButton.addAction(.init(handler: {[weak self]_ in
+            guard let self = self else {
+                return
+            }
+            self.currentIndex-=1
+            if self.currentIndex == self.UIImages.count{
+                self.currentIndex=0
+            }
+            else if self.currentIndex == -1{
+                self.currentIndex=self.UIImages.count-1
+            }
+            self.myImageView.image = self.UIImages[self.currentIndex]
+
+        }), for: .touchUpInside)
+    }
+    
     func setUpStackView(){
     myStack.axis = .vertical
        myStack.translatesAutoresizingMaskIntoConstraints = false
@@ -67,7 +122,6 @@ class DetailViewController:UIViewController{
             myStack.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -10),
                 
         ])
-        
     }
     func setUpDescription(){
         myStack.addArrangedSubview(myDescription) ///adds to stack
